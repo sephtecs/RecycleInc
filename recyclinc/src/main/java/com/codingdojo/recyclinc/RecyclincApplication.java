4ac93e103d85ad8b5936b8208204919d9e0e01da
Joseph <josephtecson18@gmail.com>
2021-10-15 13:55:27 -0700

    second commit

---
+++ recyclinc/src/main/java/com/codingdojo/recyclinc/RecyclincApplication.java
@@ -0,0 +1,13 @@
+package com.codingdojo.recyclinc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecyclincApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecyclincApplication.class, args);
+	}
+
+}
---
+++ recyclinc/src/test/java/com/codingdojo/recyclinc/RecyclincApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingdojo.recyclinc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecyclincApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
